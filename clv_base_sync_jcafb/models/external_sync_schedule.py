4cc9c308952803cd8bebd5fd78f5fe199b932bc5
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-06-18 11:01:33 -0300

    [IMP] clv_base_sync_jcafb (IMP models/external_sync_schedule.py)

--- clv_base_sync_jcafb/models/external_sync_schedule.py
+++ clv_base_sync_jcafb/models/external_sync_schedule.py
@@ -25,91 +25,93 @@ class ExternalSync(models.Model):
         from time import time
         start = time()
 
-        date_last_sync = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if (not schedule.external_disable_sync):
 
-        method_args = literal_eval(schedule.method_args)
+            date_last_sync = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        remote_object_fields = ['id', 'name', 'partner_id', 'company_id', 'tz', 'lang',
-                                'login', 'password', 'image', 'groups_id', 'active']
+            method_args = literal_eval(schedule.method_args)
 
-        # local_object_fields = ['id', 'name', 'partner_id', 'company_id',
-        #                        'login', 'password', 'image_1920', 'groups_id', 'active']
+            remote_object_fields = ['id', 'name', 'partner_id', 'company_id', 'tz', 'lang',
+                                    'login', 'password', 'image', 'groups_id', 'active']
 
-        AbstractExternalSync = self.env['clv.abstract.external_sync']
-        ResUsers = self.env['res.users']
-        ResCompany = self.env['res.company']
-        ResPartner = self.env['res.partner']
+            # local_object_fields = ['id', 'name', 'partner_id', 'company_id',
+            #                        'login', 'password', 'image_1920', 'groups_id', 'active']
 
-        external_host = schedule.external_host_id.name
-        external_dbname = schedule.external_host_id.external_dbname
-        external_user = schedule.external_host_id.external_user
-        external_user_pw = schedule.external_host_id.external_user_pw
+            AbstractExternalSync = self.env['clv.abstract.external_sync']
+            ResUsers = self.env['res.users']
+            ResCompany = self.env['res.company']
+            ResPartner = self.env['res.partner']
 
-        uid, sock, login_msg = AbstractExternalSync.external_sync_host_login(
-            external_host,
-            external_dbname,
-            external_user,
-            external_user_pw
-        )
-        schedule.external_sync_log += 'login_msg: ' + str(login_msg) + '\n\n'
+            external_host = schedule.external_host_id.name
+            external_dbname = schedule.external_host_id.external_dbname
+            external_user = schedule.external_host_id.external_user
+            external_user_pw = schedule.external_host_id.external_user_pw
 
-        if uid is not False:
+            uid, sock, login_msg = AbstractExternalSync.external_sync_host_login(
+                external_host,
+                external_dbname,
+                external_user,
+                external_user_pw
+            )
+            schedule.external_sync_log += 'login_msg: ' + str(login_msg) + '\n\n'
 
-            schedule.external_sync_log += 'Executing: "' + '_res_users_migration' + '"...\n\n'
+            if uid is not False:
 
-            remote_objects = sock.execute(external_dbname, uid, external_user_pw,
-                                          'res.users', 'search_read',
-                                          [],
-                                          remote_object_fields)
+                schedule.external_sync_log += 'Executing: "' + '_res_users_migration' + '"...\n\n'
 
-            _logger.info(u'%s %s\n', '--> remote_objects', len(remote_objects))
+                remote_objects = sock.execute(external_dbname, uid, external_user_pw,
+                                              'res.users', 'search_read',
+                                              [],
+                                              remote_object_fields)
 
-            object_count = 0
-            for remote_object in remote_objects:
+                _logger.info(u'%s %s\n', '--> remote_objects', len(remote_objects))
 
-                object_count += 1
+                object_count = 0
+                for remote_object in remote_objects:
 
-                _logger.info(u'%s %s %s', '-->', object_count, remote_object['name'])
+                    object_count += 1
 
-                local_object = ResUsers.search([('login', '=', remote_object['login'])])
+                    _logger.info(u'%s %s %s', '-->', object_count, remote_object['name'])
 
-                if local_object.id is not False:
-                    _logger.info(u'%s %s', '----->', '*** Skipped ***')
+                    local_object = ResUsers.search([('login', '=', remote_object['login'])])
 
-                else:
+                    if local_object.id is not False:
+                        _logger.info(u'%s %s', '----->', '*** Skipped ***')
 
-                    company = ResCompany.search([('name', 'in', remote_object['company_id'])])
-                    parent = ResPartner.search([('name', 'in', remote_object['company_id'])])
+                    else:
 
-                    res_user_record = {}
-                    res_user_record['name'] = remote_object['name']
-                    res_user_record['login'] = remote_object['login']
-                    res_user_record['password'] = remote_object['password']
-                    if remote_object['image'] is not False:
-                        res_user_record['image_1920'] = remote_object['image']
-                    res_user_record['lang'] = remote_object['lang']
-                    res_user_record['tz'] = remote_object['tz']
-                    res_user_record['active'] = remote_object['active']
-                    new_user = ResUsers.create(res_user_record)
+                        company = ResCompany.search([('name', 'in', remote_object['company_id'])])
+                        parent = ResPartner.search([('name', 'in', remote_object['company_id'])])
 
-                    _logger.info(u'%s %s', '----->', new_user)
+                        res_user_record = {}
+                        res_user_record['name'] = remote_object['name']
+                        res_user_record['login'] = remote_object['login']
+                        res_user_record['password'] = remote_object['password']
+                        if remote_object['image'] is not False:
+                            res_user_record['image_1920'] = remote_object['image']
+                        res_user_record['lang'] = remote_object['lang']
+                        res_user_record['tz'] = remote_object['tz']
+                        res_user_record['active'] = remote_object['active']
+                        new_user = ResUsers.create(res_user_record)
 
-                    new_partner = ResPartner.search([('id', '=', new_user.partner_id.id)])
+                        _logger.info(u'%s %s', '----->', new_user)
 
-                    _logger.info(u'%s %s', '----->', new_partner)
+                        new_partner = ResPartner.search([('id', '=', new_user.partner_id.id)])
 
-                    res_partner_record = {}
-                    res_partner_record['email'] = remote_object['login']
-                    res_partner_record['parent_id'] = parent.id
-                    res_partner_record['company_id'] = company.id
-                    new_partner.write(res_partner_record)
+                        _logger.info(u'%s %s', '----->', new_partner)
 
-        _logger.info(u'%s %s', '>>>>>>>>>> date_last_sync: ', date_last_sync)
-        _logger.info(u'%s %s', '>>>>>>>>>> Execution time: ', secondsToStr(time() - start))
+                        res_partner_record = {}
+                        res_partner_record['email'] = remote_object['login']
+                        res_partner_record['parent_id'] = parent.id
+                        res_partner_record['company_id'] = company.id
+                        new_partner.write(res_partner_record)
 
-        schedule.date_last_sync = date_last_sync
-        schedule.external_sync_log +=  \
-            'method_args: ' + str(method_args) + '\n' + \
-            'object_count: ' + str(object_count) + '\n' + \
-            'date_last_sync: ' + str(date_last_sync) + '\n' + \
-            'Execution time: ' + str(secondsToStr(time() - start)) + '\n\n'
+            _logger.info(u'%s %s', '>>>>>>>>>> date_last_sync: ', date_last_sync)
+            _logger.info(u'%s %s', '>>>>>>>>>> Execution time: ', secondsToStr(time() - start))
+
+            schedule.date_last_sync = date_last_sync
+            schedule.external_sync_log +=  \
+                'method_args: ' + str(method_args) + '\n' + \
+                'object_count: ' + str(object_count) + '\n' + \
+                'date_last_sync: ' + str(date_last_sync) + '\n' + \
+                'Execution time: ' + str(secondsToStr(time() - start)) + '\n\n'
