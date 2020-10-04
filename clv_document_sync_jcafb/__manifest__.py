b06a8f10da65452d1e9c169aa387f36a5d3c58ef
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-03 21:39:22 -0300

    [IMP] clv_document_sync_jcafb (IMP __manifest__.py)

--- clv_document_sync_jcafb/__manifest__.py
+++ clv_document_sync_jcafb/__manifest__.py
@@ -11,7 +11,8 @@
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
-        'clv_document_jcafb',
+        'clv_document',
+        # 'clv_document_jcafb',
         'clv_external_sync',
     ],
     'data': [
